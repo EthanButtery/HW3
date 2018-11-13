97e1d0e9505fecda46e8fddbafb3bce3779d62ed
Ethan Buttery <ethanbuttery@gmail.com>
2018-11-13 04:45:16 +0000

    HW3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'ethans#index'
+  root 'websites#index'
   resources :websites
   resources :ethans
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
